e30d85ce9a13e6b30dc43de614a6d53feea800a3
ksergio <113699828+ksergiocom@users.noreply.github.com>
2025-09-09 16:16:23 +0200

    remove analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,15 +9,6 @@
   </head>
 
   <body class="h-full">
-      <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-1ZNQZQPMWN"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-1ZNQZQPMWN');
-  </script>
     @routes
     @inertia
   </body>
